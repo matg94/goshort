76a581d21d196c28ee81bec9c8daeef277fe2668
mat <mat.gautron@gmail.com>
2022-02-03 17:26:02 +0000

    Added headerbar, need to add mainpage component that gets current page from it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import React from 'react';
 import URLResults from './components/URLResults';
+import HeaderBar from './pages/HeaderBar';
 import Toolbox from './components/Toolbox';
 import {Paper} from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <Paper>
-        <h1>Go Short</h1>
-      </Paper>
+      <HeaderBar/>
       <Toolbox></Toolbox>
     </div>
   );
---
+++ frontend/src/pages/HeaderBar.jsx
@@ -0,0 +1,58 @@
+import { AppBar, Button, Paper } from '@material-ui/core';
+import React from 'react';
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: "60vw",
+    minWidth: "40vw",
+    maxHeight: "15vh",
+    minHeight: "10vh",
+    margin: 5,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  button: {
+    maxWidth: "18%",
+    minWidth: "18%",
+  }
+})
+
+
+function HeaderBar(props) {
+  const classes = useStyles();
+  const [buttons, setButtons] = useState([
+    {
+      "title": "Go Short",
+      "disabled": true,    },
+    {
+      "title": "Go Custom",
+      "disabled": false,
+    },
+    {
+      "title": "Go Long",
+      "disabled": false,
+    },
+  ])
+  const handleOnClick = (index) => {
+    let newButtons = [...buttons];
+    newButtons.forEach(b => b.disabled = false)
+    newButtons[index].disabled = true
+    setButtons(newButtons)
+  }
+  return (
+    <Paper>
+      {
+        buttons.map((button, index) => {
+          console.log(`Creating button: ${index} : ${button.disabled}`)
+          return <Button key={index} variant="text" disabled={button.disabled} onClick={() => handleOnClick(index)} color="primary" className={classes.button}>{button.title}</Button>
+        })
+      }
+    </Paper>
+  );
+}
+
+export default HeaderBar;
